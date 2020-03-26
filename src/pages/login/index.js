609674b6af99ecf3587141a0e5c5dc0b465191cb
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-26 09:22:38 -0300

    Finaliza estilo da tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 import './style.css';
+import heroesImg from '../../assets/images/heroes.png';
+import logo from '../../assets/images/logo.svg';
 
 export default function Login() {
   return (
-    <h1>Hello Login</h1>
+    <div className="login">
+      <section className="form">
+        <img src={logo} alt=""/>
+        <form>
+          <h1>Faça seu Login</h1>
+          <input type="text" placeholder="Sua ID"/>
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={heroesImg} alt=""/>
+    </div>
   )
 }
